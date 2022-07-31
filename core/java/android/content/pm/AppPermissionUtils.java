58342186cec8ec5b65a4f3cca41b98f4283c25a5
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-07-31 15:22:06 +0300

    infrastructure for the Storage Scopes feature
    13: b5ff11faa09e

--- core/java/android/content/pm/AppPermissionUtils.java
+++ core/java/android/content/pm/AppPermissionUtils.java
@@ -27,6 +27,8 @@ import com.android.internal.gmscompat.GmsHooks;
 import com.android.internal.app.StorageScopesAppHooks;
 import com.android.internal.gmscompat.GmsHooks;
 
+import com.android.internal.app.StorageScopesAppHooks;
+
 /** @hide */
 @SystemApi
 public class AppPermissionUtils {
